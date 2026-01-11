ffdbb3996a964dd8a77bbd1cbf6740959c2f4c52
CursedSC <103887456+CursedSC@users.noreply.github.com>
2026-01-11 22:48:14 +0300

    Integrate new combat modules into sh_main.lua and maintain backward compatibility

--- gamemode/modules/fighting/sh_main.lua
+++ gamemode/modules/fighting/sh_main.lua
@@ -1,25 +1,49 @@
-Fight = Fight or {}
-Fight.Skills = Fight.Skills or {}
+fighting = fighting or {}
+Fight = Fight or {} -- Сохраняем для совместимости
 
-function Fight.Skills.CanUse(ply)
-    -- сюда добавить ограничения когда скилл не может юзаться
-    return true
+-- Подключение модулей новой боевой системы
+include("sh_hitbox.lua")
+include("sh_resources.lua")
+include("sh_damage.lua")
+include("sh_skill.lua")
+
+if SERVER then
+    AddCSLuaFile("sh_hitbox.lua")
+    AddCSLuaFile("sh_resources.lua")
+    AddCSLuaFile("sh_damage.lua")
+    AddCSLuaFile("sh_skill.lua")
 end
 
+-- Вспомогательные функции игрока (совместимость)
 local Player = FindMetaTable("Player")
+
+function Player:GetMana()
+    return fighting.Resources:GetMana(self)
+end
+
+function Player:GetStamina()
+    return fighting.Resources:GetStamina(self)
+end
+
+-- Система временных статусов (оставляем, если используется в других местах)
 local playerdebuff = {}
+
 function Player:AddPerStatus(name, data, time, name2)
     playerdebuff[self:Name()] = playerdebuff[self:Name()] or {}
     playerdebuff[self:Name()][name] = playerdebuff[self:Name()][name] or {}
     if name2 then 
         playerdebuff[self:Name()][name][name2] = data
         timer.Simple(time, function()
-            playerdebuff[self:Name()][name][name2] = nil
+            if IsValid(self) then
+                playerdebuff[self:Name()][name][name2] = nil
+            end
         end)
     else 
         local id = table.insert(playerdebuff[self:Name()][name], data)
         timer.Simple(time, function()
-            table.remove(playerdebuff[self:Name()][name], id)
+            if IsValid(self) then
+                table.remove(playerdebuff[self:Name()][name], id)
+            end
         end)
     end
 end
@@ -38,9 +62,4 @@ if SERVER then
     hook.Add("PlayerDeath", "RemovePerStatus", function(ply)
         playerdebuff[ply:Name()] = nil
     end)
-end
-
-
-function Player:GetMana(int)
-    return self:GetNWInt("mana", 100)
 end
